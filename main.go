0e973f83eec6086b6d40e363d7ccfd42f89d0908
River Alaqidy <ralaqidy@gmail.com>
2026-02-16 20:48:13 -0600

    updated saving data to be atomic

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"math/rand"
 	"os"
 )
 
@@ -23,18 +25,27 @@ type Iterator interface {
 	Next() (key []byte, val []byte)
 }
 
-// typical way to save and write files
+// save and write to files with atomicity
 func SaveData(path string, data []byte) error {
-	fp, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0664)
+	tmp := fmt.Sprintf("%s.tmp.%d", path, rand.Int())
+	fp, err := os.OpenFile(tmp, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0664)
 	if err != nil {
 		return err
 	}
-	defer fp.Close() // ensure file is closed after execution
+	defer func() { // discard the temp file if it still exists
+		fp.Close() // not expected to fail
+		if err != nil {
+			os.Remove(tmp)
+		}
+	}()
 
-	_, err = fp.Write(data)
-	if err != nil {
+	if _, err = fp.Write(data); err != nil { // save to temporary file
 		return err
 	}
+	if err = fp.Sync(); err != nil { // fsync
+		return err
+	}
+	err = os.Rename(tmp, path)
 	return fp.Sync() // fsync is the syscall that makes all previously written data durable is used to request and confirma durability
 }
 
